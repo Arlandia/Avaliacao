e864c150403f1d7931a73911445ff01b84f6ca6c
Arlandia <lanny.cardoso@hotmail.com>
2019-04-30 20:06:29 -0300

    Avalicao de desenvolvimento

---
+++ src/main/java/br/com/senac/controller/ControleUsuario.java
@@ -0,0 +1,8 @@
+package br.com.senac.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ControleUsuario {
+
+}
---
+++ src/main/java/br/com/senac/servico/UsuarioService.java
@@ -0,0 +1,12 @@
+package br.com.senac.servico;
+
+import org.springframework.stereotype.Service;
+
+import br.com.senac.repositorio.UsuarioRepositorio;
+
+@Service
+public class UsuarioService {
+	
+	private UsuarioRepositorio repoUsuario;
+
+}
